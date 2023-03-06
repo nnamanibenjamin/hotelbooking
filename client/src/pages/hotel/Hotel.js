ddc75db7db0d1487d9ca6a36e8f6ca7689841fb2
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-03-06 21:13:31 +0200

    some commit

--- client/src/pages/hotel/Hotel.js
+++ client/src/pages/hotel/Hotel.js
@@ -1,8 +1,9 @@
-import { LocationOnOutlined } from '@mui/icons-material'
+import { ArrowLeftOutlined, ArrowRightOutlined, CancelOutlined, LocationOnOutlined, } from '@mui/icons-material'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import MailList from '../../components/mailList/MailList'
 import Footer from '../../components/footer/Footer'
+import { useState } from 'react'
 import './hotel.css'
 
 //App ui page that will display a single hotel page
@@ -16,10 +17,10 @@ const Hotel = () => {
       src: require('../../assets/hotel1.jpg')
     },
     {
-      src: require('../../assets/hotel1.jpg')
+      src: require('../../assets/hotel.jpg')
     },
     {
-      src: require('../../assets/hotel1.jpg')
+      src: require('../../assets/hotel2.jpg')
     },
     {
       src: require('../../assets/hotel1.jpg')
@@ -32,11 +33,42 @@ const Hotel = () => {
     }
   ]
 
+  const [slideNumber, setSlideNumber] = useState(0)
+
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = (i) => {
+    setSlideNumber(i)
+    setOpen(true)
+  }
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+    if(direction === 'l'){
+      newSlideNumber = slideNumber === 0 ? 5: slideNumber - 1
+    } else {
+      newSlideNumber = slideNumber === 5 ? 0: slideNumber + 1
+    }
+
+    setSlideNumber(newSlideNumber)
+  }
+
   return (
     <div>
      <Navbar/>
      <Header type='list'/>
      <div className='hotelContainer'>
+
+      {open && <div className='slider'>
+          <CancelOutlined className='close' onClick={()=>setOpen(false)}/>
+          <ArrowLeftOutlined className='arrow' onClick={()=>handleMove('l')}/>
+
+          <div className='sliderWrapper'>
+            <img src={photos[slideNumber].src} alt='' className='sliderImg' />
+          </div>
+          <ArrowRightOutlined className='arrow'  onClick={()=>handleMove('r')}/>
+      </div>}
+
        <div className='hotelWrapper'>
         <button className='bookNow'>Book Now!</button>
         <h1 className='hotelTitle'>New Home Hotel</h1>
@@ -53,17 +85,18 @@ const Hotel = () => {
         </span>
 
         <div className='hotelImages'>
-          {photos.map(photo=>(
+          {photos.map((photo, i)=>(
             <div className='hotelImageWrapper'>
-              <img src={photo.src} alt='' className='hotelImg'/>
+              <img src={photo.src} onClick={()=>handleOpen(i)} alt='' className='hotelImg'/>
             </div>
           ))}
         </div>
+
             <div className='hotelDetails'>
               <div className='hotelDetailsText'>
                 <h1 className='hotelTitle'>Comfort and relaxation</h1>
                 <p className='hotelDesc'>
-                Excellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportvvExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airport
+                Excellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportvvExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airportExcellent location, 10mins drive from airport 
                 </p>
               </div>
 
